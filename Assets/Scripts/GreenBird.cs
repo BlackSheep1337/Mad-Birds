f784211191fb739b847a2cdbb6764eb05eabfb62
Ale Pereira <naitwa-alexandre@gmail.com>
2023-06-26 17:06:56 -0300

    script bird

--- Assets/Scripts/GreenBird.cs
+++ Assets/Scripts/GreenBird.cs
@@ -1,10 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GreenBird : MonoBehaviour
 {
 
+    private Vector3 _initialPosition;
+    private float _timeSittingAround;
+    private bool _birdWasLaunched;
+    [SerializeField] float _launchPower = 100;
+
+    private void Update()
+    {
+
+        if (_birdWasLaunched && GetComponent<Rigidbody2D>().velocity.magnitude <= 0.1)
+        {
+            _timeSittingAround += Time.deltaTime;
+        }
+
+        if (birdOutOfScreen())
+        {
+            string currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
+
+    private bool birdOutOfScreen()
+    {
+        bool result = false;
+        if (transform.position.y > 10 ||
+            transform.position.y < -10 ||
+            transform.position.x > 10 ||
+            transform.position.x < -10 ||
+            _timeSittingAround > 3
+        )
+        {
+            result = true;
+        }
+
+        return result;
+    }
+
+    private void Awake()
+    {
+        _initialPosition = transform.position;
+    }
     private void OnMouseDown()
     {
         GetComponent<SpriteRenderer>().color = Color.red;
@@ -13,6 +54,11 @@ public class GreenBird : MonoBehaviour
     private void OnMouseUp()
     {
         GetComponent<SpriteRenderer>().color = Color.white;
+        Vector2 directionToInitialPosition = _initialPosition - transform.position;
+
+        GetComponent<Rigidbody2D>().AddForce(directionToInitialPosition * _launchPower);
+        GetComponent<Rigidbody2D>().gravityScale = 1;
+        _birdWasLaunched = true;
     }
 
     private void OnMouseDrag()
